6b6d611b6578d5e8e43b62e07afc46beca4e40d1
vladislavvbulgakov <vladislavbulgakov23@gmail.com>
2026-04-05 23:07:41 +0300

    ui fix: adsViewPage details

--- client/src/entities/ad/lib/mapAdParams.ts
+++ client/src/entities/ad/lib/mapAdParams.ts
@@ -4,7 +4,18 @@ import type {
     RealEstateItemParams,
     ElectronicsItemParams,
 } from "../model/types";
-
+import {
+    REAL_ESTATE_TYPE_OPTIONS,
+    TRANSMISSION_OPTIONS,
+    ELECTRONICS_TYPE_OPTIONS,
+    CONDITION_OPTIONS,
+} from "../model/constants";
+const getLabel = (
+    value: string | undefined,
+    options: { value: string; label: string }[],
+) => {
+    return options.find((o) => o.value === value)?.label ?? "-";
+};
 export const mapAdParams = (ad: Ad): Record<string, string> => {
     switch (ad.category) {
         case "auto": {
@@ -14,7 +25,10 @@ export const mapAdParams = (ad: Ad): Record<string, string> => {
                 Бренд: p.brand ?? "-",
                 Модель: p.model ?? "-",
                 "Год выпуска": p.yearOfManufacture?.toString() ?? "-",
-                "Коробка передач": p.transmission ?? "-",
+                "Коробка передач": getLabel(
+                    p.transmission,
+                    TRANSMISSION_OPTIONS,
+                ),
                 Пробег: p.mileage?.toString() ?? "-",
                 "Мощность двигателя": p.enginePower?.toString() ?? "-",
             };
@@ -24,7 +38,7 @@ export const mapAdParams = (ad: Ad): Record<string, string> => {
             const p = ad.params as RealEstateItemParams;
 
             return {
-                "Тип недвижимости": p.type ?? "-",
+                "Тип недвижимости": getLabel(p.type, REAL_ESTATE_TYPE_OPTIONS),
                 Адрес: p.address ?? "-",
                 Площадь: p.area?.toString() ?? "-",
                 Этаж: p.floor?.toString() ?? "-",
@@ -35,10 +49,10 @@ export const mapAdParams = (ad: Ad): Record<string, string> => {
             const p = ad.params as ElectronicsItemParams;
 
             return {
-                Тип: p.type ?? "-",
+                Тип: getLabel(p.type, ELECTRONICS_TYPE_OPTIONS),
                 Бренд: p.brand ?? "-",
                 Модель: p.model ?? "-",
-                Состояние: p.condition ?? "-",
+                Состояние: getLabel(p.condition, CONDITION_OPTIONS),
                 Цвет: p.color ?? "-",
             };
         }
